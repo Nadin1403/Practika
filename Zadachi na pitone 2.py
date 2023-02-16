81ea6330409504dcd0514b5e47bfc7d6e877d353
Nadin1403 <pr1403@yandex.ru>
2023-02-16 17:46:45 +0400

    A new file added

---
+++ Zadachi na pitone 2.py
@@ -0,0 +1,32 @@
+# num = int(input())
+# if num % 2 == 0:
+#     print("чётное число", num)
+# else:
+#     print("нечётное число")
+
+# letter = input("input text")
+# if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+#     print('гласная буква')
+# elif letter == 'y':
+#     print("согласная буква и гласная буква")
+# else:
+#     print("согласная буква")
+#
+# for i in range (0, 11, 2):
+#     print(i)
+
+# for i in range (5):
+#     print(i, end=" ")
+
+# for i in range(0, 11):
+#     if i % 2 == 0:
+##         print(i, end=" ")
+#
+# for i in range (10, 0, -2):
+#     if i % 2 == 0:
+#        print(i, end=" ")
+
+# languages = ["C", "C++", "Perl", "Python"]
+# for x in languages:
+#       print(x)
+
